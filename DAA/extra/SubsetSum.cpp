d7ef83ceb692d1caa50899aa53f7c343f48e4a71
Aniket  Hend <119605156+AniketHend@users.noreply.github.com>
2023-11-14 22:31:13 +0530

    Update SubsetSum.cpp

--- DAA/extra/SubsetSum.cpp
+++ DAA/extra/SubsetSum.cpp
@@ -1,54 +1,40 @@
-#include <bits/stdc++.h>
-using namespace std;
-void solve(int i, vector<int> &arr, vector<int> temp, vector<vector<int>> &ans, int target)
-{
-    if (target == 0)
-    {
-        ans.push_back(temp);
+void solve(int i, int target, vector<int>&a, std::vector<int>arr, std::vector<vector<int>> &ans) {
+    if (!target) {
+        ans.push_back(arr);
         return;
     }
-    if (i == 0)
-    {
-        if (target == arr[0])
-        {
-            temp.push_back(arr[0]);
-            ans.push_back(temp);
-        }
+    if (i > a.size()) {
         return;
     }
-    if (target >= arr[i])
-    {
-        temp.push_back(arr[i]);
-        solve(i - 1, arr, temp, ans, target - arr[i]);
-        temp.pop_back();
-    }
-    solve(i - 1, arr, temp, ans, target);
-    return;
+    arr.push_back(a[i]);
+    solve(i + 1, target - a[i], a, arr, ans);
+    arr.pop_back();
+    solve(i + 1, target, a, arr, ans);
 }
-int main()
-{
+
+void solve() {
     int n;
-    cout << "Enter the no. of elements" << endl;
     cin >> n;
-    vector<int> arr(n);
-    vector<int> temp;
-    vector<vector<int>> ans;
-    int target = 30;
-    cout << "Enter the elements" << endl;
-    for (int i = 0; i < arr.size(); i++)
-    {
-        cin >> arr[i];
+    vector<int> a(n);
+    for (auto &i : a) {
+        cin >> i;
     }
-    cout << "Enter the target" << endl;
+
+    int target;
     cin >> target;
-    solve(n - 1, arr, temp, ans, target);
-    for (int i = 0; i < ans.size(); i++)
-    {
-        cout << "Solution No: " << i + 1 << endl;
-        for (int j = 0; j < ans[i].size(); j++)
-        {
-            cout << ans[i][j] << " ";
+
+    vector<vector<int>> ans;
+    vector<int> temp;
+    solve(0, target, a, temp, ans);
+    if (ans.size() == 0) {
+        cout << "No Subset found\n";
+        return;
+    }
+    for (auto i : ans) {
+        cout << "[";
+        for (auto j : i) {
+            cout << j << " ";
         }
-        cout << endl;
+        cout << "]\n";
     }
 }
