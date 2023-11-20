6a2146878d417aeef9462baa4c2fe7465b06330b
Aniket  Hend <119605156+AniketHend@users.noreply.github.com>
2023-11-20 10:34:49 +0530

    Update TSP.cpp

--- DAA/4/TSP.cpp
+++ DAA/4/TSP.cpp
@@ -1,146 +1,137 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define N 5
-#define INF INT_MAX
-
-class Node
-{
-public:
-    vector<pair<int, int>> path;
-    int matrix_reduced[N][N];
-    int cost;
-    int vertex;
-    int level;
+class Node {
+public :
+    int Vertex;
+    int Cost;
+    int Level;
+    vector<vector<int>> Matrix;
+    vector<pair<int, int>> Path;
 };
-
-Node* newNode(int matrix_parent[N][N], vector<pair<int, int>> const &path, int level, int i, int j) {
-    Node* node = new Node;
-    node->path = path;
+Node* newNode(vector<vector<int>>&parent, vector<pair<int, int>>&path, int level, int par, int child) {
+    Node* tmp = new Node;
+    tmp->Path = path;
     if (level != 0) {
-        node->path.push_back(make_pair(i, j));
+        tmp->Path.push_back({par, child});
     }
-    memcpy(node->matrix_reduced, matrix_parent, sizeof node->matrix_reduced);
-    for (int k = 0; level != 0 && k < N; k++) {
-        node->matrix_reduced[i][k] = INF;
-        node->matrix_reduced[k][j] = INF;
+    tmp->Matrix = parent;
+    for (int k = 0; k < parent.size() && level != 0; k++) {
+        tmp->Matrix[par][k] = INT_MAX;
+        tmp->Matrix[k][child] = INT_MAX;
     }
+    tmp->Matrix[child][0] = INT_MAX;
 
-    node->matrix_reduced[j][0] = INF;
-    node->level = level;
-    node->vertex = j;
-    return node;
+    tmp->Level = level;
+    tmp->Vertex = child;
+    return tmp;
 }
 
-void reduce_row(int matrix_reduced[N][N], int row[N]) {
-    fill_n(row, N, INF);
+void Reduce_Row(vector<vector<int>>&matrix, vector<int>&row) {
+    int n = matrix.size();
 
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            if (matrix_reduced[i][j] < row[i]) {
-                row[i] = matrix_reduced[i][j];
-            }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            row[i] = min(row[i], matrix[i][j]);
         }
     }
 
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            if (matrix_reduced[i][j] != INF && row[i] != INF) {
-                matrix_reduced[i][j] -= row[i];
-            }
-        }
-    }
-}
-
-void reduce_column(int matrix_reduced[N][N], int col[N]) {
-    fill_n(col, N, INF);
-
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            if (matrix_reduced[i][j] < col[j]) {
-                col[j] = matrix_reduced[i][j];
-            }
-        }
-    }
-
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            if (matrix_reduced[i][j] != INF && col[j] != INF) {
-                matrix_reduced[i][j] -= col[j];
+    for (int i = 0; i < n; i++) {
+        if (row[i] != INT_MAX) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] != INT_MAX) {
+                    matrix[i][j] -= row[i];
+                }
             }
         }
     }
 }
+void Reduce_Col(vector<vector<int>>&matrix, vector<int>&col) {
+    int n = matrix.size();
 
-int cost_calculation(int matrix_reduced[N][N]) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (col[j] > matrix[i][j]) {
+                col[j] = matrix[i][j];
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (col[j] != INT_MAX) {
+                if (matrix[i][j] != INT_MAX) {
+                    matrix[i][j] -= col[j];
+                }
+            }
+        }
+    }
+}
+
+int cost_calculation(vector<vector<int>>&matrix) {
+    int n = matrix.size();
+
+    vector<int> row(n, INT_MAX), col(n, INT_MAX);
+    Reduce_Row(matrix, row);
+    Reduce_Col(matrix, col);
     int cost = 0;
-    int row[N];
-    reduce_row(matrix_reduced, row);
-
-    int col[N];
-    reduce_column(matrix_reduced, col);
-
-    for (int i = 0; i < N; i++) {
-        cost += (row[i] != INT_MAX) ? row[i] : 0;
-        cost += (col[i] != INT_MAX) ? col[i] : 0;
+    for (int i = 0; i < col.size(); i++) {
+        if (row[i] != INT_MAX) cost += row[i];
+        if (col[i] != INT_MAX) cost += col[i];
     }
-
     return cost;
 }
 
-void printPath(vector<pair<int, int>> const &list) {
-    for (int i = 0; i < list.size(); i++) {
-        cout << list[i].first + 1 << " -> " << list[i].second + 1 << endl;
-    }
-}
-
-class comp {
+class cmp {
 public:
-    bool operator()(const Node* lhs, const Node* rhs) const {
-        return lhs->cost > rhs->cost;
+    bool operator()(const Node* x, const Node* y) {
+        return x->Cost > y->Cost;
     }
 };
 
-int solve(int adj[N][N]) {
+void TSP(vector<vector<int>>&adj) {
+    int n = adj.size();
+    vector<pair<int, int>>path;
 
-    priority_queue<Node*, std::vector<Node*>, comp> pq;
+    Node* root = newNode(adj, path, 0, -1, 0);
+    root->Cost = cost_calculation(adj);
 
-    vector<pair<int, int>> v;
-    Node* root = newNode(adj, v, 0, -1, 0);
-    root->cost = cost_calculation(root->matrix_reduced);
-
+    priority_queue<Node*, vector<Node*>, cmp>pq;
     pq.push(root);
+
     while (!pq.empty()) {
         Node* cur = pq.top();
         pq.pop();
-        int i = cur->vertex;
-
-        if (cur->level == N - 1) {
-            cur->path.push_back(make_pair(i, 0));
-            printPath(cur->path);
-            return cur->cost;
+        int ver = cur->Vertex;
+        cout << ver << endl;
+        if (cur->Level == n - 1) {
+            cur->Path.push_back({ver, 0});
+            cout << "Path\n";
+            // printPath(cur->Path);
+            cout << "Cost :" << cur->Cost << endl;
+            return;
         }
 
-        for (int j = 0; j < N; j++) {
-            if (cur->matrix_reduced[i][j] != INF) {
-                Node* child = newNode(cur->matrix_reduced, cur->path, cur->level + 1, i, j);
-                child->cost = cur->cost + cur->matrix_reduced[i][j] + cost_calculation(child->matrix_reduced);
+        for (int i = 0; i < n; i++) {
+            if (cur->Matrix[ver][i] != INT_MAX) {
+                Node * child = newNode(cur->Matrix, cur->Path, cur->Level + 1, ver, i);
+                child->Cost = cur->Cost +  cur->Matrix[ver][i] + cost_calculation(cur->Matrix);
+                cout << child->Cost << " " << i << endl;
                 pq.push(child);
             }
         }
-
+        cout << endl;
         delete cur;
     }
 }
-
+int INF = INT_MAX;
 int main() {
-    int adj[N][N] = {
+    vector<vector<int>> adj = {
         { INF, 20, 30, 10, 11 },
         { 15,  INF, 16,  4,  2 },
         { 3,   5,  INF, 2,  4 },
         { 19,   6,   18,   INF, 3 },
         { 16,   4,   7,   16, INF }
     };
-    cout << "\nCost is " << solve(adj);
-    return 0;
+    TSP(adj);
 }
